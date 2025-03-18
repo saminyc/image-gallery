8072c34bd18d33ee1057f29fe9fafc5643ff6490
Samin Chowdhury <SAMIN.CHOWDHURY68@qmail.cuny.edu>
2025-03-18 11:54:37 -0400

    ImageCard created

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { API_KEY } from "../config";
+import ImageCard from "./components/ImageCard";
 import "./index.css";
 
 function App() {
@@ -20,44 +21,14 @@ function App() {
   }, []);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img
-        src="https://source.unsplash.com/random"
-        alt="image"
-        className="w-full"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by Jane Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            2000
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            1000
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-400 mr-2">
-          #tag
-        </span>
-        <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-400 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-400 mr-2">
-          #tag3
-        </span>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {
+          images.map(image=>(<ImageCard key={image.id} image={image}/>))
+        }
       </div>
     </div>
-  );
+      );
 }
 
 export default App;
