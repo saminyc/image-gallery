c3b8fe04e2423fe32ff50dffa072cea0e75d281a
Samin Chowdhury <SAMIN.CHOWDHURY68@qmail.cuny.edu>
2025-03-17 21:53:11 -0400

    frontend card creation with tailwind

---
+++ app/src/App.jsx
@@ -0,0 +1,42 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+        <img src="https://source.unsplash.com/random" alt="image" className='w-full' />
+        <div className='px-6 py-4'>
+          <div className='font-bold text-purple-500 text-xl mb-2'>
+            Photo by Jane Doe
+          </div>
+          <ul>
+            <li>
+              <strong>Views: </strong>
+              4000
+            </li>
+            <li>
+              <strong>Downloads: </strong>
+              2000
+            </li>
+            <li>
+              <strong>Likes: </strong>
+              1000
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-400 mr-2">
+            #tag
+          </span>
+          <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-400 mr-2">
+            #tag2
+          </span>
+          <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-400 mr-2">
+            #tag3
+          </span>
+        </div>
+    </div>
+        )
+}
+
+export default App
